99907e58ec98b2520125a7f80875963ad9645059
mat285 <mat285@cornell.edu>
2025-06-01 16:43:02 -0400

    timeout

--- discover/ping.go
+++ discover/ping.go
@@ -9,7 +9,7 @@ import (
 
 func TCPPing(ctx context.Context, ip string, port int) error {
 	fmt.Println("Pinging IP:", ip, "on port:", port)
-	conn, err := net.DialTimeout("tcp4", fmt.Sprintf("%s:%d", ip, port), time.Second*2)
+	conn, err := net.DialTimeout("tcp4", fmt.Sprintf("%s:%d", ip, port), time.Second*5)
 	if err != nil {
 		return err
 	}
