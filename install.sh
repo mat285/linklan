af4520ea57a4904a78a08abf3bb2aaf4128efb6a
mat285 <mat285@cornell.edu>
2025-06-01 20:30:02 -0400

    exec

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-sudo curl --output /usr/local/bin/linklandaemon https://github.com/mat285/linklan/releases/download/v0.1/linklandaemon_amd64
+sudo curl --output /usr/local/bin/linklandaemon https://github.com/mat285/linklan/releases/download/v0.1.0/linklandaemon_amd64
 
 sudo chmod +x /usr/local/bin/linklandaemon
-sudo curl --output /etc/systemd/system/linklandaemon.service https://github.com/mat285/linklan/releases/download/v0.1/linklandaemon.service
+sudo curl --output /etc/systemd/system/linklandaemon.service https://github.com/mat285/linklan/releases/download/v0.1.0/linklandaemon.service
 
 sudo systemctl enable linklandaemon.service
 sudo systemctl start linklandaemon.service
