af3577f39c95a5520639741c6d3676ffffd25a92
mat285 <mat285@cornell.edu>
2025-06-01 21:11:13 -0400

    fix

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+VERSION=$1
+if [ -z "$VERSION" ]; then
+    VERSION="v0.1.0"
+fi
+
 curl -Lo linklandaemon https://github.com/mat285/linklan/releases/download/v0.1.0/linklandaemon_amd64
 sudo mv linklandaemon /bin/linklandaemon
 sudo chown root:root /bin/linklandaemon
@@ -9,7 +14,6 @@ sudo chmod a+x /bin/linklandaemon
 
 curl -Lo linklandaemon.service https://github.com/mat285/linklan/releases/download/v0.1.0/linklandaemon.service
 sudo mv linklandaemon.service /etc/systemd/system/linklandaemon.service
-# sudo chmod 0755 /etc/systemd/system/linklandaemon.service
 sudo chmod 644 /etc/systemd/system/linklandaemon.service
 sudo chown root:root /etc/systemd/system/linklandaemon.service
 
