2e93dc6151fd19af0602ba57357162175fd15379
mat285 <mat285@cornell.edu>
2025-06-08 09:09:18 -0400

    fix

--- install.sh
+++ install.sh
@@ -36,12 +36,12 @@ sudo systemctl stop linklandaemon.service || true
 sudo systemctl disable linklandaemon.service || true
 echo "Installing LinkLan daemon version ${VERSION}..."
 
-curl -Lo linklandaemon https://github.com/mat285/linklan/releases/download/${VERSION}/linklandaemon_${OS}_${ARCH}
+curl --fail-with-body -Lo linklandaemon https://github.com/mat285/linklan/releases/download/${VERSION}/linklandaemon_${OS}_${ARCH}
 sudo mv linklandaemon /bin/linklandaemon
 sudo chown root:root /bin/linklandaemon
 sudo chmod a+x /bin/linklandaemon
 
-curl -Lo linklandaemon.service https://github.com/mat285/linklan/releases/download/${VERSION}/linklandaemon.service
+curl --fail-with-body -Lo linklandaemon.service https://github.com/mat285/linklan/releases/download/${VERSION}/linklandaemon.service
 sudo mv linklandaemon.service /etc/systemd/system/linklandaemon.service
 sudo chmod 644 /etc/systemd/system/linklandaemon.service
 sudo chown root:root /etc/systemd/system/linklandaemon.service
