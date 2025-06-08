93db9d391d5d32959dce6146aa00189ad1238c96
mat285 <mat285@cornell.edu>
2025-06-08 08:46:40 -0400

    fix

--- build.sh
+++ build.sh
@@ -6,6 +6,12 @@ BUILD_DIR=build
 
 rm -rf ${BUILD_DIR}/ || true
 
-GOOS=linux GOARCH=amd64 go build -o ${BUILD_DIR}/linklandaemon_${GOOS}_${GOARCH} cmd/daemon/main.go
-GOOS=linux GOARCH=arm64 go build -o ${BUILD_DIR}/linklandaemon_${GOOS}_${GOARCH} cmd/daemon/main.go
-GOOS=darwin GOARCH=arm64 go build -o ${BUILD_DIR}/linklandaemon_${GOOS}_${GOARCH} cmd/daemon/main.go
+export GOOS=linux 
+export GOARCH=amd64
+go build -o ${BUILD_DIR}/linklandaemon_${GOOS}_${GOARCH} cmd/daemon/main.go
+export GOOS=linux 
+export GOARCH=arm64
+go build -o ${BUILD_DIR}/linklandaemon_${GOOS}_${GOARCH} cmd/daemon/main.go
+export GOOS=darwin
+export GOARCH=arm64
+go build -o ${BUILD_DIR}/linklandaemon_${GOOS}_${GOARCH} cmd/daemon/main.go
