f09d41518d273b7fdd43382e27baa39c51bad5fe
mat285 <mat285@cornell.edu>
2025-06-14 18:59:06 -0400

    fix

--- cmd/update/main.go
+++ cmd/update/main.go
@@ -33,11 +33,14 @@ func main() {
 	// }
 
 	wg := &sync.WaitGroup{}
+	lock := &sync.Mutex{}
 	for _, machine := range machines {
 		wg.Add(1)
 		go func(machine string) {
 			defer wg.Done()
+			lock.Lock()
 			fmt.Printf("Updating %s...\n", machine)
+			lock.Unlock()
 			cmd := exec.Command("ssh",
 				"-A",
 				machine,
@@ -46,15 +49,22 @@ func main() {
 				// `sudo sh -c 'mkdir -p /etc/prometheus/node_exporter/textfile_collector'`,
 			)
 			cmd.Env = append(os.Environ(), `SUDO_OPTS="-S"`)
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			cmd.Stdin = os.Stdin
-			if err := cmd.Run(); err != nil {
+			if err != nil {
+				fmt.Printf("Error creating pipe for %s: %v\n", machine, err)
+				os.Exit(1)
+			}
+			output, err := cmd.CombinedOutput()
+			lock.Lock()
+			fmt.Println(string(output))
+			lock.Unlock()
+			if err != nil {
 				fmt.Printf("Error running command on %s: %v\n", machine, err)
 				os.Exit(1)
 			}
+			lock.Lock()
+			fmt.Printf("Successfully updated %s\n", machine)
+			lock.Unlock()
 		}(machine)
-		fmt.Printf("Successfully updated %s\n", machine)
 	}
 	wg.Wait()
 }
